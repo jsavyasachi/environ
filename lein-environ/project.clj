288947592f4ceb1e7d0b8b4d5e0864ddc9c88d12
Savyasachi <jsavyasachi@gmail.com>
2026-06-06 09:13:24 -0700

    build(lein-environ): coordinate to net.clojars.savya/lein-environ 1.3.0
    Point :url at the fork. No dependency changes: robert.hooke and
    leiningen.core.main come from Leiningen (:eval-in-leiningen true).

--- lein-environ/project.clj
+++ lein-environ/project.clj
@@ -1,6 +1,6 @@
-(defproject lein-environ "1.2.0"
+(defproject net.clojars.savya/lein-environ "1.3.0"
   :description "Leiningen plugin for Environ"
-  :url "https://github.com/weavejester/environ"
+  :url "https://github.com/jsavyasachi/environ"
   :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
